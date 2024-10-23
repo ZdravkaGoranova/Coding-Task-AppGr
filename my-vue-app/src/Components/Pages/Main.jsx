9d5d1d91138c9894b72dfd3ddc2b82cc12a39cb5
zdravkaGoranova <zdr_mladenova90@abv.bg>
2024-10-23 13:27:10 +0300

    install uuid

--- my-vue-app/src/Components/Pages/Main.jsx
+++ my-vue-app/src/Components/Pages/Main.jsx
@@ -1,13 +1,100 @@
+import { useEffect, useState } from "react";
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { v4 as uuid } from 'uuid';
+
+
 import CreateNote from "../../features/CreateNote";
 import EditNote from "../../features/EditNote";
 
+
+
 const Main = () => {
+    const [inputTitle, setInputTitle] = useState('');
+    const [inputContent, setInputContent] = useState('');
+    const [notes, setNotes] = useState([]);
+    const [currentEditNite, setcurrentEditNite] = useState('');
+
+
+    const fetchNotes = async () => {
+        try {
+            const data = JSON.parse(localStorage.getItem('Cooding Task'))
+            if (data) {
+                setNotes(data)
+                console.log(data);
+            }
+            setNotes(data)
+        } catch (error) {
+            console.log(`Errrot:`, error);
+            toast.error("Error fetching Notes !", error);
+        }
+    }
+    const saveNote = async (note) => {
+console.log(note);
+
+        try {
+            if (note) {
+                const newNote = await note;
+                setNotes([...notes], newNote);
+                toast.success("The Note save successfully !");
+            }
+
+        } catch (error) {
+            console.log(`Errrot:`, error);
+            toast.error("Error save Note !", error);
+        }
+
+    }
+
+    useEffect(() => {
+        fetchNotes()
+    }, [])
+
+    useEffect(() => {
+        window.localStorage.setItem('Cooding Task', JSON.stringify(notes))
+    }, [notes])
+
+    const saveHandler = (e) => {
+        e.preventDefault();
+
+        if (inputTitle.trim() == "") {
+
+            toast.error("Input Title is required!");
+            return;
+        } if (inputContent.trim() == "") {
+            toast.error("Input Content is required!");
+            return;
+        }
+
+        if (inputTitle.trim() !== "" && inputContent.trim() !== "") {
+            const newNote = {
+                id: uuid(),
+                key: uuid(),
+                title: inputTitle,
+                content: inputContent,
+            }
+            console.log(newNote);
+            
+            saveNote(newNote);
+            setInputTitle('');
+            setInputContent('');
+        }
+    }
     return (
-     <>
-     <Note></Note>
-     <CreateNote></CreateNote>
-     <EditNote></EditNote>
-     </>   
+        <>
+
+            <CreateNote
+                inputTitle={inputTitle}
+                setInputTitle={setInputTitle}
+                inputContent={inputContent}
+                setInputContent={setInputContent}
+                saveHandler={saveHandler}
+            ></CreateNote>
+
+            <EditNote></EditNote>
+            <ToastContainer />
+        </>
     );
 };
 
--- my-vue-app/src/features/CreateNote.jsx
+++ my-vue-app/src/features/CreateNote.jsx
@@ -1,5 +1,39 @@
-const CreateNote = () => {
-    return (<h1>CreateNote</h1>);
-  };
-  
-  export default CreateNote;
+const CreateNote = ({
+  inputTitle,
+  setInputTitle,
+  inputContent,
+  setInputContent,
+  saveHandler
+
+}) => {
+  return (
+    <>
+      <form action="#"className="create-note">
+        <div className="form-group">
+          <label htmlFor="title">Note title:</label>
+          <input type="text" id='title' name='title' value={inputTitle} placeholder="Enter title"
+            onChange={(e) => {
+              e.preventDefault()
+              console.log(e.target.value);
+              setInputTitle(e.target.value);
+            }} />
+        </div>
+
+        <textarea id="content" name="content" rows="4" cols="50"
+          value={inputContent} placeholder="Enter content..."
+          onChange={(e) => {
+            e.preventDefault()
+            console.log(e.target.value);
+            setInputContent(e.target.value)
+          }}>
+          At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.
+        </textarea>
+        <button type="submit" className="btn btn-primary" 
+        onClick={saveHandler}>Save</button>
+      </form>
+
+
+    </>);
+};
+
+export default CreateNote;
