3599bbabae69c99eb2eedb4c50877dcf6576bb85
zdravkaGoranova <zdr_mladenova90@abv.bg>
2024-10-23 12:07:34 +0300

    creating new Components Header,Footer, Main

---
+++ my-vue-app/src/Components/Pages/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    const year= new Date().getFullYear();
+    return (
+      <footer>
+        <p>Copyright {year} © </p>
+        <p> Created by Zdravka Goranova </p>
+      </footer>
+    );
+  }
+  
+  export default Footer
---
+++ my-vue-app/src/Components/Pages/Main.jsx
@@ -0,0 +1,14 @@
+import CreateNote from "../../features/CreateNote";
+import EditNote from "../../features/EditNote";
+
+const Main = () => {
+    return (
+     <>
+     <Note></Note>
+     <CreateNote></CreateNote>
+     <EditNote></EditNote>
+     </>   
+    );
+};
+
+export default Main;
